2df35b3d8e69bb36f3898d30ed0e1f7e510e9d0f
Jorge Hiroki <jorge.h.c.s.jh@gmail.com>
2023-07-06 23:45:39 -0300

    Modificações na venda

--- Pizzaria/Pizza/carrinho.php
+++ Pizzaria/Pizza/carrinho.php
@@ -17,7 +17,7 @@ if(isset($_POST['cancelar'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta lang="PT-BR">
-    <title>Loja de Informática</title>
+    <title>Pizzaria Felipe Massas</title>
     <link rel="sortcut icon" href="Imagens/logo_loja.png" type="image/png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
